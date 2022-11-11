a6f86d1e76790a15786d0afdb85f2e8ff0163f82
mmendell <105883493+mmendell@users.noreply.github.com>
2022-11-11 12:37:21 -0500

    task 6.2 added both login, register components

--- src/app/fetch-api-data.service.spec.ts
+++ src/app/fetch-api-data.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { FetchApiDataService } from './fetch-api-data.service';
+import { FetchApiDataServie } from './fetch-api-data.service';
 
 describe('FetchApiDataService', () => {
-  let service: FetchApiDataService;
+  let service: FetchApiDataServie;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(FetchApiDataService);
+    service = TestBed.inject(FetchApiDataServie);
   });
 
   it('should be created', () => {
