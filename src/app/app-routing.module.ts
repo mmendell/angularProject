b2987b034e3a7882581ecb0d675f9acbf076c198
mmendell <105883493+mmendell@users.noreply.github.com>
2022-11-14 17:48:11 -0500

    task 6.4 got build workingh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BookCardComponent } from './book-card/book-card.component';
-import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
-const appRoutes: Routes = [
-  { path: 'welcome', component: WelcomePageComponent },
-  { path: 'books', component: BookCardComponent},
-  { path: '', redirectTo: 'welcome', pathMatch: 'prefix'},
-];
+const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
-  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -12,17 +15,18 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+
+
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { BookCardComponent } from './book-card/book-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
-import { MatIconModule } from '@angular/material/icon';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AuthorComponent } from './author/author.component';
 import { GenreComponent } from './genre/genre.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
-import { Routes } from '@angular/router';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 const appRoutes: Routes = [
@@ -46,20 +50,19 @@ const appRoutes: Routes = [
     NavBarComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    MatDialogModule,
-    MatButtonModule,
     MatInputModule,
+    MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
+    MatDialogModule,
     MatSnackBarModule,
-    MatIconModule,
-    NavBarComponent
+    FormsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
